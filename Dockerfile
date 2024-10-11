2a678fd372921c2ca6f7c4bbce67b3ebb6a0f091
Aryan Wadhawan <aryan.xxvii@gmail.com>
2024-10-11 15:08:04 +0530

    dockerfile, aws config

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM python:3.12-slim
+
+ENV PYTHONDONTWRITEBYTECODE=1
+ENV PYTHONUNBUFFERED=1
+
+# Install AWS CLI
+RUN apt-get update && apt-get install -y \
+    awscli \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+
+WORKDIR /app
+
+COPY requirements.txt .
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
+
+EXPOSE 8080
+
+CMD ["python", "src/clearbg/main.py"]  # Update with your actual entry point
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from clearbg import logger
 from clearbg.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from clearbg.pipeline.stage_02_model_trainer import ModelTrainingPipeline
+from clearbg.pipeline.stage_03_mlflow_pipeline import MlflowEval
 
 
 STAGE_NAME = "Data Ingestion Pipeline"
@@ -25,6 +26,20 @@ try:
 
     model_trainer = ModelTrainingPipeline()
     model_trainer.main()
+
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Evaluation"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+
+    model_eval = MlflowEval()
+    model_eval.main()
 
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 
